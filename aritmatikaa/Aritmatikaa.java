2fcfb3f9d478174bddcccf278d73515e12c93cb1
aripraypga <117058249+aripraypga@users.noreply.github.com>
2022-11-07 11:23:06 +0800

    upload tugas

---
+++ aritmatikaa/Aritmatikaa.java
@@ -0,0 +1,36 @@
+
+package aritmatikaa;
+
+/**
+ *
+ * @author nama dan nim
+ */
+public class Aritmatikaa {
+ //property
+    private double hasil;
+    
+//method tambah
+    public void tambah(double bil1,double bil2){
+        
+        this.hasil = bil1 + bil2;
+        
+    }
+    // method kurang
+    public void kurang (double bil1,double bil2){
+        this.hasil = bil1 - bil2;
+    }
+    //method kali
+    public void kali(double bil1,double bil2){
+      
+        this.hasil = bil1 * bil2;
+    }
+    //method bagi
+    public void bagi(double bil1,double bil2){
+        this.hasil = bil1/bil2;
+    }
+    //get hasil 
+    public double gethasil(){
+        return this.hasil;
+    }
+    }
+    
